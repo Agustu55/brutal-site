0951f9c7a8c748d7b590cdd0717620693f631c58
Agustu55 <gusdstevens@gmail.com>
2020-10-23 18:22:29 -0600

    chose a gdamn gradient array

--- index.php
+++ index.php
@@ -21,11 +21,14 @@
 
     <!-- change the src on clicks -->
     <img onclick="randomImg()" id="hidden-content" width="100%" src="img/hrz1.jpg">
-    <img onclick="randomImg()" id="content" width="100%" src="img/img2.jpg">
+    <img onclick="randomImg()" id="content" width="100%" src="img/img1.jpg">
     <div id="text"><a href="real.html">ENTER</a></div>
     <img onclick="randomImg()" id="logo" src="obj/obj1.png">
 
-    <script type="text/javascript" src="js/loadRandomImg.js"></script>
+    <div onclick="randomImg()" id="square">
+      <p id="hexText"></p>
+    </div>
 
+    <script type="text/javascript" src="js/loadRandomImg.js"></script>
   </body>
 </html>
--- js/loadRandomImg.js
+++ js/loadRandomImg.js
@@ -1,7 +1,51 @@
+var colors = [
+  "#c471ed",
+  "#f64f59",
+  "#FBD786",
+  "#f7797d",
+  "#f953c6",
+  "#8E2DE2",
+  "#91EAE4",
+  "#38ef7d",
+  "#6A82FB",
+  "#ff5e62",
+  "#CF8BF3",
+  "#FDB99B",
+  "#FFC371",
+  "#92FE9D",
+  "#fc00ff",
+  "#00dbde",
+  "#5433FF",
+  "#A5FECB"
+];
+
+// returns a 3 item array with direction color1 and color2 for a gradient
+function randomGradient() {
+  var direction = Math.floor(Math.random() * 361);
+  var color1 = colors[Math.floor(Math.random() * colors.length)];
+  var color2 = colors[Math.floor(Math.random() * colors.length)];
+
+
+  // this will ensure that no two are the same
+  while (color1 == color2) {
+    color1 = colors[Math.floor(Math.random() * colors.length)];
+  }
+
+  return [direction,color1,color2]
+}
+
 
 // function to update source to a random image
 function randomImg(){
-   document.getElementById('logo').src = "obj/obj" + Math.round(Math.random()*9+1) + ".png";
-   document.getElementById('content').src = "img/img" + Math.round(Math.random()*32+1) + ".jpg";
-   document.getElementById('hidden-content').src = "img/hrz" + Math.round(Math.random()*11+1) + ".jpg";
+   document.getElementById('logo').src = "obj/obj" + Math.floor(Math.random() * 10)+1 + ".png";
+   document.getElementById('content').src = "img/img" + (Math.floor(Math.random() * 33)+1) + ".jpg";
+   document.getElementById('hidden-content').src = "img/hrz" + (Math.floor(Math.random() * 12)+1) + ".jpg";
+
+   // to randomly change the background gradient
+   var gradient = randomGradient();
+   document.getElementById('square').style.background = "linear-gradient("+gradient[0] + "deg," + gradient[1] + "," + gradient[2] +")";
+
+   // this will add txt to the gradient for debugging -- just make sure to get rid of the angle
+   // document.getElementById('hexText').innerHTML = String(gradient[1])+","+String(gradient[2]);
+
 }
